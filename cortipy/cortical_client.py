191acdcbd3a0a9c2cdca2af6a763618b18511215
Alexander Lavin <alavin@numenta.com>
2015-05-03 18:32:29 -0700

    functions filled in, cleanup still needed

--- cortipy/cortical_client.py
+++ cortipy/cortical_client.py
@@ -23,6 +23,7 @@
 
 import hashlib
 import os
+import random
 import requests
 
 try:
@@ -35,9 +36,10 @@ DEFAULT_BASE_URL = "http://api.cortical.io/rest"
 DEFAULT_RETINA = "en_synonymous"
 DEFAULT_CACHE_DIR = "/tmp/cortipy"
 DEFAULT_VERBOSITY = 0
+DEFAULT_FILL_SDR = "random"
 DEFAULT_PARTOFPSEECH = None
 
-# A retina is the Cortical.io word space model:
+# A retina is the cortical.io word space model:
 # http://documentation.cortical.io/retinas.html#the-retinas
 RETINA_SIZES = {
       "en_synonymous": {
@@ -54,7 +56,7 @@ TARGET_SPARSITY = 0.03
 
 
 
-class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
+class CorticalClient():
   """
   Main class for making calls to the REST API.
   """
@@ -83,6 +85,7 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
 
 
   def _callAPI(self, resourcePath, method, queryParams, postData, headers={}):
+  ## TO DO: unit test a good response, for both GET and POST, has statu_code==200
     url = self.apiUrl + resourcePath
     headers['api-key'] = self.apiKey
     response = None
@@ -101,6 +104,8 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
     return response
 
 
+## TO DO: make a generic caching function, or delete:
+
 #  def _writeToCache(self, data, type):
 #    # Create a cache location for the data, where it will be fetched from.
 #    cachePath = os.path.join(self.cacheDir,
@@ -115,8 +120,32 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
 #    return data
 
 
+  def _placeholderFingerprint(self, string, option):
+    """
+    When the API returns a null fingerprint, fill with a random or empty bitmap.
+    
+    We seed the random number generator such that a given string will yield the
+    same fingerprint each time this function is called.
+    Saving the internal state of the generator reduces the likelihood of
+    repeating values from previous inputs.
+    """
+    if option == "random":
+      size = RETINA_SIZES[self.retina]
+      total = int(float(size["width"]) * float(size["height"]))
+      state = random.getstate()
+      random.seed(string)
+      fingerprint = random.sample(xrange(total), int(total*TARGET_SPARSITY))
+      random.setstate(state)
+      return {"positions":fingerprint}
+        ## TO DO: test if these need to be sorted... if so, use fingerprint.sort()
+        ## TO DO: unit test that this returns the same fingerprint for a given term, and dif fingerprints for dif terms
+    else:
+      return {"positions":[]}
+
+
   def getBitmap(self, term):
-    # -> Will this also work for larger pieces of text? If not, need to use Text api
+    ## -> Will this also work for larger pieces of text? If not, need to use Text api
+    ## TO DO: ^^ unit test
     """
     For the input term, return the SDR info; either from cache or the REST API.
     
@@ -162,33 +191,33 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
                                  "get_fingerprint":True
                                  },
                                postData=None)
-
       responseObj = json.loads(response.content)
-#      import pdb; pdb.set_trace()
+
       if isinstance(responseObj, list) and len(responseObj)>0:
         sdrInfo = responseObj[0]
-#      elif responseObj==[]:
-#        # no fingerprint generated
-#        "==> Why am I here???"  ## Remove if this never happens
-#        import pdb; pdb.set_trace()
-#        sdrInfo = {"positions": []}
       else:
-        sdrInfo["term"]=term
-        sdrInfo["score"]=None
-        sdrInfo["pos_types"]=None
-        sdrInfo["fingerprint"]={"positions":[]}
+        if self.verbosity > 0:
+          print ("API could not return info for the text \'%s\'. Perhaps the "
+                "text includes punctuation that should be ignored by the "
+                "tokenizer.\nGenerating a placeholder fingerprint for \'%s\'..."
+                % (term, term))
+        # score and pos_types keys are empty b/c not querying the API
+        sdrInfo["score"] = None
+        sdrInfo["pos_types"] = None
+        sdrInfo["term"] = term
+        sdrInfo["fingerprint"] = self._placeholderFingerprint(term, DEFAULT_FILL_SDR)
+      
+      # Include values for SDR dimensions and sparsity.
       if (not "width" in sdrInfo) or (not "height" in sdrInfo):
         size = RETINA_SIZES[self.retina]
         sdrInfo["width"] = size["width"]
         sdrInfo["height"] = size["height"]
-
-#      import pdb; pdb.set_trace()
-      # attach the sparsity for reference
       total = float(sdrInfo["width"]) * float(sdrInfo["height"])
       on = len(sdrInfo["fingerprint"]["positions"])
       sparsity = round((on / total) * 100)
       sdrInfo["sparsity"] = sparsity
       ## TO DO: unit test (and raise exception here?) for sparsity w/in range of TARGET_SPARSITY
+        # and unit test the width, heigh, sparsity, fingerprint[positions] fields not null; b/c these are necessary in terms.py
 
       # write to cache
       with open(cachePath, 'w') as f:
@@ -226,10 +255,9 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
       if self.verbosity > 0:
         print "\tfetching raw (similar) terms from REST API"
       # include part of speech?
+      pos = None
       if DEFAULT_PARTOFPSEECH:
         pos = data["pos_types"]
-      else:
-        pos = None
       response = self._callAPI(resourcePath="/expressions/similar_terms",
                                method="POST",
                                headers={"Accept": "Application/json",
@@ -249,7 +277,6 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
         f.write(response.content)
       responseObj = json.loads(response.content)
 
-#    import pdb; pdb.set_trace()
     # Return terms in a readable format
     similar = []
     for term in responseObj:
@@ -259,7 +286,7 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
     return similar
 
 
-  def tokenize(self, text): ## TODO - only text, no fingerprint... python-client-sdk/textApi.py line 65
+  def tokenize(self, text): ## TODO: unit test this works, and ignores specific grams
     """Get a list of sentence tokens from a text string.
     
     @param text     (str)               Text string to tokenize
@@ -278,20 +305,27 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
     if os.path.exists(cachePath):
       with open(cachePath) as cacheFile:
         response = json.load(cacheFile)
-#    else:
-
-
+    else:
+      response = self._callAPI(resourcePath="/text/tokenize",
+                               method="POST",
+                               headers={"Accept": "Application/json",
+                                 "Content-Type": "application/json"},
+                               queryParams = {
+                                 "retina_name":self.retina,
+                                 "POStags":POStags
+                                 },
+                               postData=text)
+      # Cache the response data
       with open(cachePath, 'w') as f:
         json.dump(response, f)
-
+      import pdb; pdb.set_trace()  ## check out what is being returned
     return [sentence.split(",") for sentence in response]
 
 
-  def compare(self, bitmap1, bitmap2): ## TODO
+  def compare(self, bitmap1, bitmap2): ## TODO: test that the input data structure is correct, and return dict is as commented
     """
-    Given two bitmaps, return their comparison, i.e. a dict with the CEPT
-    comparison metrics.
-    Here's an example return dict:
+    Given two bitmaps, return their comparison, i.e. a dict with the REST
+    comparison metrics. Here's an example return dict:
       {
         "Cosine-Similarity": 0.6666666666666666,
         "Euclidean-Distance": 0.3333333333333333,
@@ -305,10 +339,23 @@ class CorticalClient(): ## TO DO: move rest of pycept/cept.py over
       }
     """
     
-    pass
+    data = json.dumps(
+      [
+        {"positions": bitmap1},
+        {"positions": bitmap2}
+      ]
+    )
+    response = self._callAPI(resourcePath="/compare",
+                             method="POST",
+                             headers={"Accept": "Application/json",
+                                      "Content-Type": "application/json"},
+                             queryParams = {"retina_name":self.retina},
+                             postData=data)
+    
+    return json.loads(response.content)
 
 
-  def getSdr(self, term):
+  def getSdr(self, term):  ## Move to utils (fluent utils?)
     bitmap = self.getBitmap(term)
     size = bitmap["width"] * bitmap["height"]
     positions = bitmap["positions"]
